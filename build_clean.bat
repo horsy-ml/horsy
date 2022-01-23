99bf4cc57ad320a65b7da12c3088dda98de91e3c
BarsTiger <kit.barsic@gmail.com>
2022-01-23 22:30:55 +0200

    Changed file system

--- build_clean.bat
+++ build_clean.bat
@@ -1,4 +1,6 @@
 pip install -r requirements.txt
+rmdir /s /q build
 pyinstaller --noconfirm --icon "img/icon.ico" --console --onefile "horsy.py"
 rmdir /s /q __pycache__
 del horsy.spec
+rmdir /s /q build
