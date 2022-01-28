a44e77e709dc43b04d0c53dfa17db939841dd443
BarsTiger <kit.barsic@gmail.com>
2022-01-28 16:49:10 +0200

    GUI development

--- dist.bat
+++
@@ -1,3 +0,0 @@
-xcopy /s /Y dist\horsy.exe bin\horsy.exe*
-xcopy /s /Y dist\horsygui.exe bin\horsygui.exe*
-xcopy /s /Y dist\horsy_installer.exe bin\installer-horsy-win.exe*
---
+++ dist.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("xcopy /s /Y dist\horsy.exe bin\horsy.exe*")
+os.system("xcopy /s /Y dist\horsygui.exe bin\horsygui.exe*")
+os.system("xcopy /s /Y dist\horsy_installer.exe bin\installer-horsy-win.exe*")
+
+with open("web_vars/version", "r") as f_r:
+    version = f_r.readline()
+
+with open("web_vars/version", "w") as f_w:
+    f_w.write(str(int(version) + 1))
