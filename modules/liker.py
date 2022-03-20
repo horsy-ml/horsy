751742ad8b378a33b43900df14765895d87c8404
BarsTiger <kit.barsic@gmail.com>
2022-03-20 15:01:10 +0200

    Refactored liker

--- modules/liker.py
+++ modules/liker.py
@@ -4,20 +4,17 @@ from modules.auth import get_auth
 
 
 def like(package, is_gui=False, login_ui=None, Ui_LoginWindow=None):
-    body = {
-        "auth": get_auth(is_gui, login_ui, Ui_LoginWindow),
-        "rate": 1,
-        "packageName": package
-    }
-    r = requests.post(f"{horsy_vars.protocol}{horsy_vars.server_url}/packages/rate", json=body).json()
-    print(r["message"])
-    return r["message"]
+    return send(package, 1, is_gui, login_ui, Ui_LoginWindow)
 
 
 def dislike(package, is_gui=False, login_ui=None, Ui_LoginWindow=None):
+    return send(package, 0, is_gui, login_ui, Ui_LoginWindow)
+
+
+def send(package, type, is_gui=False, login_ui=None, Ui_LoginWindow=None):
     body = {
         "auth": get_auth(is_gui, login_ui, Ui_LoginWindow),
-        "rate": 0,
+        "rate": type,
         "packageName": package
     }
     r = requests.post(f"{horsy_vars.protocol}{horsy_vars.server_url}/packages/rate", json=body).json()
