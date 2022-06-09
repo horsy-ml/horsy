568a380326849d6f0695b57bcd27a6aae4a2f375
BarsTiger <kit.barsic@gmail.com>
2022-06-09 10:24:08 +0300

    now waiting for server answer in new thread

--- ui/modules/items_expander.py
+++ ui/modules/items_expander.py
@@ -1,7 +1,14 @@
+import threading
 from ui.gui import Ui_MainWindow
 from modules.search import info
+from qt_thread_updater import get_updater
+
+
+def on_installed_click_(ui: Ui_MainWindow) -> None:
+    ui.installed_packages_from_list_lay.show()
+    get_updater().call_latest(ui.installed_package_desc.setText, 'Loading...')
+    get_updater().call_latest(ui.installed_package_desc.setText, info(ui.installed_packages_list.currentItem().text()))
 
 
 def on_installed_click(ui: Ui_MainWindow) -> None:
-    ui.installed_packages_from_list_lay.show()
-    ui.installed_package_desc.setText(info(ui.installed_packages_list.currentItem().text()))
+    threading.Thread(target=on_installed_click_, args=(ui,)).start()
