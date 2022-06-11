919231fe0c3cb6e0c5e608eeb6dfe3f9d2a81d58
BarsTiger <kit.barsic@gmail.com>
2022-06-11 15:15:45 +0300

    now horsypath var not needed

--- modules/core/vars.py
+++ modules/core/vars.py
@@ -1,6 +1,10 @@
 import os
+import sys
 
 
 protocol = "https://"
 server_url = 'horsy.ml'
-horsypath = os.popen('echo %HORSYPATH%').read().replace('\n', '') + '\\'
+horsypath = (os.path.dirname(sys.executable) + '\\') \
+    if "python.exe" not in sys.executable \
+    else os.path.expanduser("~") + '\\horsy\\' if os.path.exists(os.path.expanduser("~") + '\\horsy') \
+    else (lambda: (os.mkdir(os.path.expanduser("~") + '\\horsy'), os.path.expanduser("~") + '\\horsy\\'))()[1]
